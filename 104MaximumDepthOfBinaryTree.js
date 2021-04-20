5fd7bd6ee7f53a256c13b09efb36fe352817305b
Jiande Li <alexlijd@gmail.com>
2021-04-19 22:54:15 -0400

    104renew

--- 104MaximumDepthOfBinaryTree.js
+++ 104MaximumDepthOfBinaryTree.js
@@ -1,4 +1,5 @@
- * Definition for a binary tree node.
+/* 
+ *Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
