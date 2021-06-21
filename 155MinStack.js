49b319cd6801912666cd71e8dc2109c2bba2f13a
Jiande Li <alexlijd@gmail.com>
2021-06-21 17:02:09 -0400

    155 import

--- 155MinStack.js
+++ 155MinStack.js
@@ -1,8 +1,13 @@
 /**
+
+Time complexity:O(1)
+Space complexity: O(1)
  * initialize your data structure here.
  */
 var MinStack = function() {
     this.stack=[]
+    this.min=[]
+    this.l=0
 };
 
 /** 
@@ -10,6 +15,12 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(val) {
+    if(this.l<=0){
+        this.min.push(val)
+    }else{
+    (val<this.min[this.l-1] ? this.min.push(val) : this.min.push(this.min[this.l-1]))
+    }
+    this.l+=1
     this.stack.push(val)
 };
 
@@ -17,8 +28,11 @@ MinStack.prototype.push = function(val) {
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    if(this.stack.length>0){
-        return this.stack.splice(this.stack.length-1,1)
+    if(this.l>0){
+        this.min.splice(this.stack.length-1,1)
+        let last = this.stack.splice(this.l-1,1)
+        this.l-=1
+        return last
     }
     return
 };
@@ -28,7 +42,7 @@ MinStack.prototype.pop = function() {
  */
 MinStack.prototype.top = function() {
     if(this.stack.length>0){
-        return this.stack[this.stack.length-1]
+        return this.stack[this.l-1]
     }
     return
 };
@@ -36,6 +50,54 @@ MinStack.prototype.top = function() {
 /**
  * @return {number}
  */
+MinStack.prototype.getMin = function() {
+    if(this.l>0){
+     return this.min[this.l-1]
+    }
+    return
+};
+
+/** 
+Time complexity:O(n)
+Space complexity: O(1)
+
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+
+/*
+
+var MinStack = function() {
+    this.stack=[]
+};
+
+
+MinStack.prototype.push = function(val) {
+    this.stack.push(val)
+};
+
+
+MinStack.prototype.pop = function() {
+    if(this.stack.length>0){
+        return this.stack.splice(this.stack.length-1,1)
+    }
+    return
+};
+
+
+MinStack.prototype.top = function() {
+    if(this.stack.length>0){
+        return this.stack[this.stack.length-1]
+    }
+    return
+};
+
+
 MinStack.prototype.getMin = function() {
     if(this.stack.length>0){
      return Math.min(...this.stack)
